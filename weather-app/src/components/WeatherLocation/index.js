3eab4f57a1a6ecf899985d0d1cdca588193ed45d
raulsoissa <raulsoissa@gmail.com>
2020-04-09 01:03:37 -0400

    iniciando video 50

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
+import convert from 'convert-units';
 import WeatherData from './WeatherData/index';
 import Location from './Location';
 import './styles.css';
 import {
-    CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY,
+    SUN
 } from './../../constants/weathers';
+
+const location = "Santiago,cl"
+const api_key = "adb866c0cb646005a389d2f8e9268761";
+const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
 
 const data = {
     temperature: 25,
     weatherState: SUN,
-    humidity: 10,
-    wind: '10 m/s',
-}
-
-const data2 = {
-    temperature: 15,
-    weatherState: WINDY,
     humidity: 10,
     wind: '10 m/s',
 }
@@ -29,11 +28,40 @@ class WeatherLocation extends Component {
             data: data,
         };
     }
+    getTemp = kelvin => {
+        return Number(convert(kelvin).from("K").to("C").toFixed(2));
+    }
+
+    getWeatherState = weather_data => {
+        return SUN;
+    }
+
+    getData = weather_data => {
+        const { humidity, temp } = weather_data.main;
+        const { speed } = weather_data.wind;
+        const weatherState = this.getWeatherState(weather_data);
+        const temperature = this.getTemp(temp);
+
+        const data = {
+            humidity,
+            temperature,
+            weatherState,
+            wind: `${speed} m/s`,
+        }
+
+        return data;
+    }
     handleUpdateClick = () => {
-        console.log("actualizado");
-        this.setState({
-            city: 'San Vicente',
-            data: data2,
+        fetch(api_weather).then( resolve => {
+            return resolve.json();
+        }).then( data => {
+            
+            const newWeather = this.getData(data);
+            console.log(newWeather);
+            debugger;
+            this.setState({
+                data: newWeather
+            });
         });
     }
     render() {
